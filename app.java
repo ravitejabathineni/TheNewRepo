2fa42a84bbd3e553c17c0fb1cced00cc40428936
ravitejabathineni <ravitejabathineni@gmail.com>
2026-06-05 19:05:32 +0530

    2nd developer updated the code app.java

--- app.java
+++ app.java
@@ -50,3 +50,22 @@ public class Main {
         }
     }
 }
+This is second developer code 
+   import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
+}
